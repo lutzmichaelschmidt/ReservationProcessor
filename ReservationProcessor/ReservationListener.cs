578b738b8167df582287ac2e7b2a267b402a0401
lutzmichaelschmidt <61434839+lutzmichaelschmidt@users.noreply.github.com>
2020-04-02 17:21:11 +0000

    Add project files.

---
+++ ReservationProcessor/ReservationListener.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMqUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReservationProcessor
+{
+    class ReservationListener : RabbitListener
+    {
+        private ILogger<ReservationListener> logger;
+
+
+    }
+}
---
+++ ReservationProcessor/ReservationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReservationProcessor
+{
+    public class ReservationModel
+    {
+        public int Id { get; set; }
+        public string For { get; set; }
+        public DateTime ReservationCreated { get; set; }
+        public string Books { get; set; }
+        public string Status { get; set; }
+    }
+}
